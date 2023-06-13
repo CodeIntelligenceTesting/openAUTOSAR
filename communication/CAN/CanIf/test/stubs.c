029940d1e185f0c35b4d8adaa76fd99be60021be
psy <patrice.salathe@code-intelligence.com>
2023-06-13 17:45:27 +0200

    Cleans up module fuzzing setup

--- communication/CAN/CanIf/test/stubs.c
+++ communication/CAN/CanIf/test/stubs.c
@@ -1,8 +1,7 @@
 #include "Can.h"
-#include "Com_Com.h"
+#include "Com.h"
 
 // Can module stubs
-
 const Can_ControllerConfigType CanControllerConfigData[1];
 const Can_ConfigSetType CanConfigSetData;
 
@@ -18,12 +17,10 @@ Can_ReturnType Can_Write( Can_Arc_HTHType hth, Can_PduType *pduInfo ) {
 
 
 // Com module stubs
-
 void Com_RxIndication(PduIdType ComRxPduId, const PduInfoType* PduInfoPtr) {}
 
 void Com_TxConfirmation(PduIdType ComTxPduId) {}
 
 
 // Det module stubs
-
 void Det_ReportError(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId) {}
